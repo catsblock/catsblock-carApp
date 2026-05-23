ad5af50b77fd767c295aa5d085d0944e65aa9826
catsblock <castilhoisaac2@gmail.com>
2026-05-22 22:25:41 -0300

    Add androidx.browser dependency
    Added androidx.browser dependency for secure login.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.browser:browser:1.8.0") // Essencial para login seguro
 }
